dff07b7a8a23150c00831e0bb566170449b97c5c
Jose Centeno <centeno.jose.manuel@gmail.com>
2017-04-02 15:55:11 -0700

    separated the code to calibrate the camera and created an intermidiate pickle file to hold the calibration matrix and dist values

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,6 +1,6 @@
 #!/home/jcenteno/miniconda3/envs/carnd-term1/bin/python
 
-
+import pickle
 import numpy as np
 import cv2
 import glob
@@ -24,15 +24,12 @@ def chessboard_draw(img, nx, ny):
     return img
 
 
-def cal_undistort(img, objpoints, imgpoints):
+def cal_undistort(img, mtx, dist):
     """ 
     cal_undistort takes an image, object points, and image points
     performs the camera calibration, image distortion correction and 
     returns the undistorted image
     """
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    image_shape = (img.shape[1], img.shape[0])
-    mtx, dist = calibrate_cam(objpoints, imgpoints, image_shape)
     undist = cv2.undistort(img, mtx, dist, None, mtx)  
     return undist
 
@@ -45,37 +42,6 @@ def calibrate_cam(objpoints, imgpoints, img_shape):
 
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_shape, None, None)
     return mtx, dist
-
-
-def warper(img, src, dst):
-    # Compute and apply perpective transform
-    img_size = (img.shape[1], img.shape[0])
-    M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
-    return warped
-
-def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
-    
-    # Apply the following steps to img
-    # 1) Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # 2) Take the derivative in x or y given orient = 'x' or 'y'
-    if orient == 'x':
-        deriv_tuple = (1, 0)
-    else:
-        deriv_tuple = (0, 1)
-    sob = cv2.Sobel(gray, cv2.CV_64F, deriv_tuple[0], deriv_tuple[1])
-    # 3) Take the absolute value of the derivative or gradient
-    abs_sobel = np.absolute(sob)
-    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
-    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
-    # 5) Create a mask of 1's where the scaled gradient magnitude 
-            # is > thresh_min and < thresh_max
-    binary_output = np.zeros_like(scaled_sobel)
-    binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
-
-    # 6) Return this mask as your binary_output image
-    return binary_output
 
 
 def find_image_points(cal_image_file_names, object_points,  display_images = True):
@@ -102,15 +68,6 @@ def find_image_points(cal_image_file_names, object_points,  display_images = Tru
     return np.array(obj_p_list), np.array(img_p_list)
 
 
-def lane_finding_pipeline(image):
-    return image
-
-def process_video(video_in, video_out, pipeline):
-    clip1 = VideoFileClip(video_in)
-    white_clip = clip1.fl_image(pipeline)
-    white_clip.write_videofile(video_out, audio=False)
-
-
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0), ...
 #                             (0,1,0), (1,1,0), (2,1,0), ...
 #                                                      ...., (8,5,0)
@@ -125,15 +82,23 @@ imgpoints = [] # 2d points in image plane.
 # Make a list of calibration images
 cal_images = glob.glob('./camera_cal/calibration*.jpg')
 objpoints, imgpoints = find_image_points(cal_images, objp, False)
+first_image = cv2.imread('./camera_cal/calibration1.jpg')  
+image_shape = (first_image.shape[1], first_image.shape[0])
+mtx, dist = calibrate_cam(objpoints, imgpoints, image_shape)
+calibration_parameters = {"mtx":mtx, "dist":dist}
 
-for image_name in glob.glob('./test_images/*.jpg'):
+#Undistort all test images
+for image_name in glob.glob('./test_images/test*.jpg'):
     image = cv2.imread(image_name)    
-    undistorted = cal_undistort(image, objpoints, imgpoints)
+    undistorted = cal_undistort(image, mtx, dist)
 
     output_name = "./output_images/" + image_name.split('/')[-1]
     cv2.imwrite(output_name, undistorted)
     #cv2.imshow('img',undistorted)
     cv2.waitKey(500)
 
+#store calibration coeff:
 
-process_video('project_video.mp4', 'project_video_out.mp4', lane_finding_pipeline)
+with open("./camera_cal_out/calibration_parameters.p", 'wb') as p_out:
+    # Pickle the 'data' dictionary using the highest protocol available.
+    pickle.dump(calibration_parameters, p_out)
