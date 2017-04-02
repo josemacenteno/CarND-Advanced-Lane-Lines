c527fd403d768b742b5cdb562129b8802b8a93f4
Jose Centeno <centeno.jose.manuel@gmail.com>
2017-04-02 01:55:53 -0700

    gathered some code

---
+++ examples/useful_code.py
@@ -0,0 +1,106 @@
+
+
+# Gray scale transform
+gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+
+#Sobel gradient detection (similat to canny, but in a single direction)
+sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+
+
+# Convert sobel results into an image:
+abs_sobelx = np.absolute(sobelx)
+scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+#Apply threshold to an image
+thresh_min = 20
+thresh_max = 100
+sxbinary = np.zeros_like(scaled_sobel)
+sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+plt.imshow(sxbinary, cmap='gray')
+
+
+
+
+# Read in an image and grayscale it
+image = mpimg.imread('signs_vehicles_xygrad.png')
+
+# Define a function that applies Sobel x or y, 
+# then takes an absolute value and applies a threshold.
+# Note: calling your function with orient='x', thresh_min=5, thresh_max=100
+# should produce output like the example image shown above this quiz.
+def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
+    
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the derivative in x or y given orient = 'x' or 'y'
+    if orient == 'x':
+        deriv_tuple = (1, 0)
+    else:
+        deriv_tuple = (0, 1)
+    sob = cv2.Sobel(gray, cv2.CV_64F, deriv_tuple[0], deriv_tuple[1])
+    # 3) Take the absolute value of the derivative or gradient
+    abs_sobel = np.absolute(sob)
+    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    # 5) Create a mask of 1's where the scaled gradient magnitude 
+            # is > thresh_min and < thresh_max
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+
+    # 6) Return this mask as your binary_output image
+    binary_output = np.copy(sxbinary) # Remove this line
+    return binary_output
+    
+# Run the function
+grad_binary = abs_sobel_thresh(image, orient='x', thresh_min=20, thresh_max=100)
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(grad_binary, cmap='gray')
+ax2.set_title('Thresholded Gradient', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+
+
+
+#Calibrate Camera Quiz:
+import pickle
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Read in the saved objpoints and imgpoints
+dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
+objpoints = dist_pickle["objpoints"]
+imgpoints = dist_pickle["imgpoints"]
+
+# Read in an image
+img = cv2.imread('test_image.png')
+
+# TODO: Write a function that takes an image, object points, and image points
+# performs the camera calibration, image distortion correction and 
+# returns the undistorted image
+def cal_undistort(img, objpoints, imgpoints):
+    # Use cv2.calibrateCamera() and cv2.undistort()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    undist = cv2.undistort(img, mtx, dist, None, mtx)  
+    return undist
+
+undistorted = cal_undistort(img, objpoints, imgpoints)
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(undistorted)
+ax2.set_title('Undistorted Image', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
